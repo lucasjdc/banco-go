6db4d2c0824e5662f7cf59587a22a2ef933f2a8b
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-12-18 21:26:00 -0300

    Alterando a visibilidade

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"banco/contas"
-	"banco/clientes"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{"Bruno", "123.456.789-12", "Professor"}
-	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(150)
 	
-	fmt.Println(contaDoBruno)
- 
+	fmt.Println(contaExemplo.ObterSaldo()) 
 }
