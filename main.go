46220fc7e720bdd882adbcd457021789c0c552b7
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-12-20 21:01:23 -0300

    Interface

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
 	"banco/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)	
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string	
+}
+
 func main() {
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(1000)
+	PagarBoleto(&contaDoDenis, 60)
 	
 	fmt.Println(contaDoDenis.ObterSaldo())
+	
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 100)
+	
+	fmt.Println(contaDaLuisa.ObterSaldo())
 }
